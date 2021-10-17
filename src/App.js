2444b3c0f01ceba88b46fcca7aa6f59274ed90d9
Orifjon Yunusjonov <mryediniofficial9924@gmail.com>
2021-10-17 15:07:06 +0500

    all files added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect, useState} from "react";
+import axios from "axios";
+import SockJS from "sockjs-client";
+// import Stomp from "stompjs"
+var Stomp = require("stompjs");
 function App() {
+  const [state,setState] = useState({})
+  useEffect(()=> {
+    console.log("cdm")
+
+  },[])
+  const onSubmit = () =>{
+    axios.post("http://localhost:8093/api/v1/websocket/send",{event:"SOME_TEST",content:"write from my "}).then(
+        res=>console.log(res)
+    ).catch(error=>console.error(error))
+    onConnect();
+  }
+  const onConnect = () => {
+    const soc = new SockJS("http://localhost:8093/mb-websocket");
+    const stompClient = Stomp.over(soc);
+    stompClient.connect({}, function (frame) {
+      console.log("Connected: " + frame);
+      stompClient.subscribe("/topic/messages", function (msg) {
+        console.log("Salom", JSON.parse(msg.body));
+      });
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    Helllo
+
+      {/*<input type="text" placeholder={"write some"}/>*/}
+      <input type="submit" onClick={()=>onSubmit()}/>
+      <p>1</p>
+      {}
+    </>
   );
 }
 
